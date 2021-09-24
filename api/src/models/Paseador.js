3f6724950647bf7f4e8bb48731d8cb8b1129f546
Marcelo Nieto <marcelonieto90@hotmail.com>
2021-09-24 16:11:50 -0300

    Initial Commit

---
+++ api/src/models/Paseador.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      life_span: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.TEXT,
+      },
+    },
+    { timestamps: false }
+  );
+};
