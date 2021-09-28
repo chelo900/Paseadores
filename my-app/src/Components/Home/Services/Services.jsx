5c8bca1a6b942ae9b8c409f9f6b3862e03df63e1
Felipeballarino <80782336+Felipeballarino@users.noreply.github.com>
2021-09-28 11:23:46 -0300

    landign page (#3)

---
+++ my-app/src/Components/Home/Services/Services.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import style from './Services.module.css'
+import foto1 from '../../../media/foto1Service.jpg'
+import foto2 from '../../../media/foto2Service.jpg'
+
+const Services = () => {
+    return (
+        <div className={style.container} id='services'>
+            <div className={style.title}>
+                <h1>Services</h1>
+            </div>
+            <div className={style.servicios}>
+                <div className={style.servConteiner}>
+                    <div className={style.serv}>
+                        <img className={style.imagen} src={foto1} alt=''/>
+                        <h3>Paseo</h3>
+                        <p>Registate como paseador, coloca tus datos, y espera a que te conctacten.</p>
+                        <p>Registate como paseador, coloca tus datos, y espera a que te conctacten.</p>
+                        
+                    </div>
+                </div>
+                <div className={style.servConteiner}>
+                   <div className={style.serv} >
+                        <img  className={style.imagen} src={foto2} alt=''/>
+                        <h3>Cuidado</h3>
+                        <p>Registate como paseador, coloca tus datos, y espera a que te conctacten.</p>
+                        <p>Registate como paseador, coloca tus datos, y espera a que te conctacten.</p>
+                      
+                   </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Services
