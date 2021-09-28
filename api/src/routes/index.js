d8c4ac4055d189d54b838a7c0daf7e2acd5fb3ec
cepentivolpi <ce.pentivolpi@gmail.com>
2021-09-28 17:50:03 -0300

    rutas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,18 +1,8 @@
 const { Router } = require("express");
-const axios = require("axios").default;
-const { User } = require("../db");
+const get_details = require ("./get_details")
 
 const router = Router();
 
-router.get("/Paseador/:id", async (req, res) => {
-  const { id } = req.params.id;
-  try {
-    const datos = await User.findByPk(id);
-    const detallesDatos = { name: datos.name, surname: datos.surname };
-    res.status(200).send(detallesDatos);
-  } catch (error) {
-    if (error.response?.status === 404) {
-      res.status(404).send("No se encontró al paseador");
-    } else res.status(500).send("Ecurrió un error");
-  }
-});
+router.use("/walkers", get_details);
+
+module.exports = router;
