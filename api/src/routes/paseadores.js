d0e843dc55570de132512bccae4be4259d9dd5a0
cepentivolpi <ce.pentivolpi@gmail.com>
2021-09-28 14:39:38 -0300

    Creacion routes/paseadores.js

---
+++ api/src/routes/paseadores.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const {Walker } = require('../db')
+const router = Router();
+
+router.get("/:id", async (req, res) => {
+    const {id} = req.params.id
+    try{  
+            const datos = await Walker.findByPk(id)
+            const detallesDatos = {name : datos.name, surname: datos.surname} 
+            res.status(200).send(detallesDatos);
+        }
+    catch (error) {
+        if (error.response?.status === 404) {
+          res.status(404).send("No se encontró al paseador");
+        } else res.status(500).send("Ecurrió un error");
+      }
+    });
+
+    module.exports = router;
