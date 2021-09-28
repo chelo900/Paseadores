ecd870864ce34a9adec8b1585eaa510fdb1ccd18
Martina-dP <martudelpino@gmail.com>
2021-09-28 20:58:12 -0300

    formulario

--- my-app/src/Components/Home/Form/form.js
+++ my-app/src/Components/Home/Form/form.js
@@ -8,7 +8,6 @@ const [input, setInput] = useState({
     surname : "",
     image : "",
     phone : "" ,
-    ubication : "",
     email : "",
     carer : "",
     age : "",
@@ -20,90 +19,117 @@ return (
         <form >
             <div>
                 <label> Image Profile : </label>
+                    <input 
+                        type="file" 
+                        accept=".jpg, .png, .pdf"
+                        value = {input.image}
+                    />
             </div>
             <div>
-                <label> DNI frente : </label>
+                <label> Photo of identity document in front : </label>
+                    <select>
+                        <option> DNI </option>
+                        <option> Cédula de identidad </option>
+                        <option> Cédula de ciudadanía </option>
+                        <option> Documento único de identidad </option>
+                        <option> Cédula de identidad Civil </option>
+                        <option> Pasaporte </option>
+                    </select>
+                <input 
+                    type="file" 
+                    accept=".jpg, .png, .pdf"
+                    value = {input.dni}
+                />
             </div>
             <div>
-                <label> DNI atras : </label>
+                <label> Photo of Identity document back : </label>
+                    <select>
+                        <option> DNI </option>
+                        <option> Cédula de identidad </option>
+                        <option> Cédula de ciudadanía </option>
+                        <option> Documento único de identidad </option>
+                        <option> Cédula de identidad Civil </option>
+                        <option> Pasaporte </option>
+                    </select>
+                <input 
+                    type="file" 
+                    accept=".jpg, .png, .pdf"
+                    value = {input.dni}
+                />
             </div>
             <div>
                 <label> Name : </label>
-                <input 
-                type = "text"
-                placeholder = "Name"
-                value = {input.name}
-                name = "name"
-                />
+                    <input 
+                    type = "text"
+                    placeholder = "Name"
+                    value = {input.name}
+                    name = "name"
+                    />
             </div>
             <div>
                 <label> SurName : </label>
-                <input 
-                type = "text"
-                placeholder = "SurName"
-                value = {input.surname}
-                name = "surname"
-                />
+                    <input 
+                    type = "text"
+                    placeholder = "SurName"
+                    value = {input.surname}
+                    name = "surname"
+                    />
             </div>
             <div>
                 <label> Age : </label>
-                <input 
-                type = "text"
-                placeholder = "Age"
-                value = {input.age}
-                name = "Age"
+                    <input 
+                    type = "text"
+                    placeholder = "Age"
+                    value = {input.age}
+                    name = "Age"
                 />
             </div>
             <div>
                 <label> Date of Birth : </label>
-                <input 
-                type = "text"
-                placeholder = "dd/mm/aa"
-                value = {input.dateofBirth}
-                name = "date of birth"
-                />
+                    <input 
+                    type = "text"
+                    placeholder = "dd/mm/aa"
+                    value = {input.dateofBirth}
+                    name = "date of birth"
+                    />
             </div>
             <div>
                 <label> Phone : </label>
-                <input 
-                type = "number"
-                placeholder = "ej: +54 11 68525749"
-                value = {input.phone}
-                name = "phone"
-                />
+                    <input 
+                    type = "number"
+                    placeholder = "ej: +54 11 68525749"
+                    value = {input.phone}
+                    name = "phone"
+                    />
             </div>
             <div>
                 <label> Email : </label>
-                <input 
-                type = "text"
-                placeholder = "paseador@gmail.com"
-                value = {input.email}
-                name = "email"
-                />
+                    <input 
+                    type = "text"
+                    placeholder = "paseador@gmail.com"
+                    value = {input.email}
+                    name = "email"
+                    />
             </div>
             <div>
                 <label> Password : </label>
-                <input 
-                type = "text"
-                placeholder = "Password123"
-                value = {input}
-                name = "contraseña"
-                />
-            </div>
-            <div>
-                <label> Ubication : </label>
-                <input 
-                type = "text"
-                placeholder = "ej: Palermo"
-                value = {input.ubication}
-                name = "ubicacion"
-                />
+                    <input 
+                    type = "text"
+                    placeholder = "Password123"
+                    value = {input.password}
+                    name = "contraseña"
+                    />
             </div>
             <div>
                 <label> Carer : </label>
-                <input />
+                <select>
+                    <option> Select </option>
+                    <option> Walker </option>
+                    <option> Carer </option>
+                    <option> Walker and Carer</option>
+                </select>
             </div>
-            <Link to = "/home">
+            <Link to = "/">
                 <button type = "submit"> Create User </button>
             </Link>
         </form>
