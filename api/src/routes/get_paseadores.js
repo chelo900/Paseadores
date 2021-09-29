3f885c928c08b69a0a54a27d7b16584c9372f650
Chelo <86071721+chelo900@users.noreply.github.com>
2021-09-29 15:51:56 -0300

    pagination (#21)

--- api/src/routes/get_paseadores.js
+++ api/src/routes/get_paseadores.js
@@ -3,39 +3,40 @@ const { User } = require("../db");
 
 const router = Router();
 
-
-
 router.get("/", async (req, res) => {
-    
-    try{
-        
-        const allActiveWalkers = await User.findAll({
-            where: {
-                status: "active"
-            }
-        })
-        const allActiveWalkersCards = await allActiveWalkers.map(w=>{
-            return {
-                id: w.id,
-                name: w.name,
-                surname: w.surname,
-                image : w.image,
-                service: w.service ,
-                ubication: w.ubication,
-                reputation: w.reputation,
-                price: w.price,
-            }
-        })
-        if(allActiveWalkersCards){
-
-            res.status(200).send(allActiveWalkersCards)
-        }else{
-        res.status(404).send("Not found");
-        }
-    }
-    catch{
-         res.status(500).send("Error");
-      }
+  const { page, results } = req.query;
+  try {
+    const allActiveWalkers = await User.findAll({
+      where: {
+        status: "active",
+      },
     });
+    console.log(allActiveWalkers);
+    const allActiveWalkersCards = await allActiveWalkers.map((w) => {
+      return {
+        id: w.id,
+        name: w.name,
+        surname: w.surname,
+        image: w.image,
+        service: w.service,
+        ubication: w.ubication,
+        reputation: w.reputation,
+        price: w.price,
+      };
+    });
+    if (allActiveWalkersCards) {
+      const lastElement = Number(page) * Number(results);
+      const firstElement = (Number(page) - 1) * Number(results);
 
-    module.exports = router;
+      const paginated = allActiveWalkersCards.slice(firstElement, lastElement);
+
+      res.status(200).send(paginated);
+    } else {
+      res.status(404).send("Not found");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
