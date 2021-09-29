91ebb576af624fcc2802d65382003e400fbeef34
cepentivolpi <ce.pentivolpi@gmail.com>
2021-09-29 18:47:04 -0300

    Put datos del perfil

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const get_details = require("./get_details");
 const postUser = require("./postUser");
 const putDetailsUser = require ("./putDetailsUser")
+const putDetailsProfile = require ("./putDetailsProfile")
 const get_paseadores = require("./get_paseadores");
 
 const router = Router();
@@ -10,5 +11,6 @@ router.use("/walkers", get_details);
 router.use("/allActiveWalkers", get_paseadores);
 router.use("/createUser", postUser);
 router.use("/updateuser", putDetailsUser)
+router.use("/updateuserProfile", putDetailsProfile)
 
 module.exports = router;
