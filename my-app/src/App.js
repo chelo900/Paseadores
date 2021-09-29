47d4867e05a128c4b90e59147d7d1229b109900c
Nicolas <76431460+Edenoor@users.noreply.github.com>
2021-09-29 15:38:16 -0300

    Log in (#20)
    * Log in
    
    * LogIn
    
    Co-authored-by: Chelo <86071721+chelo900@users.noreply.github.com>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Components/Home/home'
-import Login from './Components/Login/Login'
+
+import {BrowserRouter as Router} from 'react-router-dom'
+import Routes from './Routes';
+
+
 
 const App = () => {
 
-  return (
-    <BrowserRouter>
-      <div className='app'>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/createUser' />
-          <Route exact path='/login' component={Login}/>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  )
+    return (
+        <Router>
+          <Routes />
+        </Router>
+  
+    )
+
 }
 
 export default App
