56594b2c9cf8502cda6e03c299064e3fcb2f4361
Ger06 <gerardo.kohatus@gmail.com>
2021-09-29 20:35:24 -0300

    actions, reducer, store, indexprovider

---
+++ my-app/src/actions/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export function getAllPaseadores(){
+    try {
+        return async function(dispatch){
+            return axios.get ('http://localhost:3001/allActiveWalkers?page=1')
+            .then (paseadores=>{
+                dispatch({
+                    type: 'GET_PASEADORES',
+                    payload: paseadores.data
+                })
+            })
+        }
+    } catch (error) {
+        console.log (error)
+    }
+}
+
+// export function newPaseador (payload){
+//     try {
+//         return async function (dispatch){
+//             return axios.post ("http://localhost:3001/createuser", payload)
+//             .then(paseador=>{
+//                 dispatch({
+//                     type: 'NEW_PASEADOR',
+//                     payload: paseador.data
+//                 })
+//             })
+//         }
+//     } catch (error) {
+//         console.log (error)
+//     }
+// }
---
+++ my-app/src/reducer/index.js
@@ -0,0 +1,22 @@
+const initialState = {
+    allPaseadores : []
+}
+
+function rootReducer(state = initialState, action) {
+    switch(action.type){
+        case 'GET_PASEADORES':
+             return {
+                 ...state,
+                 allPaseadores: action.payload
+            }
+        // case 'NEW_PASEADOR':
+        //     return {
+        //         ...state
+        //     }
+
+    default: return state
+    }
+    
+}
+
+export default rootReducer;
