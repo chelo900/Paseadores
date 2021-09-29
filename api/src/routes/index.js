e7db3330a68dd693f14e857e3f014878a486ee82
Chelo <86071721+chelo900@users.noreply.github.com>
2021-09-28 21:06:01 -0300

    postUser route created (#12)
    okey

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const get_details = require ("./get_details")
-
+const get_details = require("./get_details");
+const postUser = require("./postUser");
 const router = Router();
 
 router.use("/walkers", get_details);
+router.use("/createUser", postUser);
 
 module.exports = router;
