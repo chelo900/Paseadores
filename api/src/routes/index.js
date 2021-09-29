1abc6413bf97f08578b072ed6bdc2036a6851aa7
Ger06 <gerardo.kohatus@gmail.com>
2021-09-29 13:43:47 -0300

    Ruta put detalle paseador

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const get_details = require("./get_details");
 const postUser = require("./postUser");
+const putDetailsUser = require ("./putDetailsUser")
 const router = Router();
 
 router.use("/walkers", get_details);
 router.use("/createUser", postUser);
+router.use("/updateuser", putDetailsUser)
 
 module.exports = router;
