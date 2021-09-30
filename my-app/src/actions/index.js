f96ff963ab60b26251914f89dfeba8a5c1424474
Martina-dP <martudelpino@gmail.com>
2021-09-30 19:03:19 -0300

    action-reducer

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+export const GET_PASEADORES = "GET_PASEADORES"
+export const GET_PASEADOR_FOR_ID = "GET_PASEADOR_FOR_ID"
+// export const NEW_PASEADOR = "NEW_PASEADOR"
+export const PUT_DETAILS_PROFILE = "PUT_DETAILS_PROFILE"
+export const PUT_DETAILS_USER = "PUT_DETAILS_USER"
+export const ORDER = "ORDER"
+export const FILTER_PRICE = "FILTER_PRICE"
+export const FILTER_SCHEDULE = "FILTER_SCHEDULE"
+export const FILTER_UBICATION = "FILTER_UBICATION"
+
 export function getAllPaseadores(){
     try {
         return async function(dispatch, page){
@@ -79,3 +89,51 @@ export function newPaseador (payload){
          console.log (error)
      }
  }
+
+ export function Order(order, attribute) {
+    return async function (dispatch) {
+      return axios .get(`http://localhost:3001/allActiveWalkers/${attribute}/${order}`)
+        .then((paseador) => {
+          dispatch({
+            type: "ORDER",
+            payload: paseador.data,
+          });
+        });
+    };
+  }
+  
+  export function FilterPrice ( price ) {
+    return async function (dispatch) {
+      return axios .get(`http://localhost:3001/allActiveWalkers/filter/${price}`)
+        .then((paseador) => {
+          dispatch({
+            type: "FILTER_PRICE",
+            payload: paseador.data,
+          });
+        });
+    };
+  }
+
+  export function FilterSchedule ( schedule ) {
+    return async function (dispatch) {
+      return axios .get(`http://localhost:3001/allActiveWalkers/filter/${schedule}`)
+        .then((paseador) => {
+          dispatch({
+            type: "FILTER_SCHEDULE",
+            payload: paseador.data,
+          });
+        });
+    };
+  }
+
+  export function FilterUbication( ubication ) {
+    return async function (dispatch) {
+      return axios .get(`http://localhost:3001/allActiveWalkers/filter/${ubication}`)
+        .then((paseador) => {
+          dispatch({
+            type: "FILTER_UBICATION",
+            payload: paseador.data,
+          });
+        });
+    };
+  }
