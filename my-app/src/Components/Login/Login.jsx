62af31815b22797711578c01a38bb8a5ad5b077b
unknown <nicoolasbeltrame@gmail.com>
2021-09-30 18:59:13 -0300

    Login 2.0

--- my-app/src/Components/Login/Login.jsx
+++ my-app/src/Components/Login/Login.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import style from "./Login.module.css"
 import { Link } from "react-router-dom";
+import Log from "./Google_Auth";
+import { useSelector } from "react-redux";
+
+
 const Login = () => {
+
+    // const dispatch = useDispatch()
+    // const [values, setValues] = useState({
+    //     email: '',
+    //     password: ''
+    // })
+
+    // const handleOnChange = ({target : {name, value}}) => setValues({
+    //     ...values,
+    //     [name]: value
+    // })
+    // const handleOnSubmit = e => {
+    //     e.preventDefault()
+    //     console.log('Values:', values)
+    //     dispatch(createUser(values));
+    //     setValues({
+    //         email: '',
+    //         password: ''
+    //     });
+    //     alert('User created successfully')
+    // };
+
+    const user = useSelector(state => state)
+
+    console.log(user)
+
     return (
         <div className={style.container}>
             <div className={style.log}>
@@ -21,10 +53,13 @@ const Login = () => {
                     <input className={style.login} type="submit" value="login"></input>
                     <div className={style.link}>
                         <span>Not registered?
-                            <Link className={style.create} to='/createWalker'>
+                            <Link className={style.create} to='/pre-login'>
                                 <span className={style.create}>Create account</span>
                             </Link>
                         </span>
+                    </div>
+                    <div className={style.google}>
+                    <Log />
                     </div>
                 </form>
             </div>
