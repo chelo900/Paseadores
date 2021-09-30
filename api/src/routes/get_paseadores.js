44e79aa7433a9c52ec9ee4dcd7d92c6e3b72026f
Goncervan <gonfedecer@gmail.com>
2021-09-30 13:00:14 -0300

    ruteo del form

--- api/src/routes/get_paseadores.js
+++ api/src/routes/get_paseadores.js
@@ -4,9 +4,8 @@ const { User } = require("../db");
 const router = Router();
 
 router.get("/", async (req, res) => {
-
+  const {name} = req.params
   try {
-
     const allActiveWalkers = await User.findAll({
       where: {
         status: "active"
@@ -25,23 +24,28 @@ router.get("/", async (req, res) => {
       }
     })
     if (allActiveWalkersCards) {
+      //GET BY NAME
+      if (name) {
+        try {
+          const nameSearch = allActiveWalkersCards.filter(
+            (user) => user.name.includes(name) || user.surname.includes(name)
+          );
+          res.status(200).send(nameSearch);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      // PAGINATION
+      const results = 5;
+      const lastElement = Number(page) * results;
+      const firstElement = (Number(page) - 1) * results;
 
-      res.status(200).send(allActiveWalkersCards)
+      const paginated = allActiveWalkersCards.slice(firstElement, lastElement);
+      res.status(200).send(paginated);
     } else {
       res.status(404).send("Not found");
     }
-  }
-      // PAGINATION
-      const results = 5;
-  const lastElement = Number(page) * Number(results);
-  const firstElement = (Number(page) - 1) * Number(results);
-
-  const paginated = allActiveWalkersCards.slice(firstElement, lastElement);
-  res.status(200).send(paginated);
-    else {
-    res.status(404).send("Not found");
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
   }
 });
