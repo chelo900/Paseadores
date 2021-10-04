1a342fde3bc53b97d3cbe9259a323e97b1c33852
cepentivolpi <ce.pentivolpi@gmail.com>
2021-10-03 23:23:31 -0300

    Busqueda por ubicación con todas las ubicaciones y van quedando las que coinciden con lo que se escribe

---
+++ api/src/routes/get_ubication.js
@@ -0,0 +1,51 @@
+const { Router } = require("express");
+const { User } = require("../db");
+const { Op } = require("sequelize");
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+    const ubication = req.query.ubication
+    console.log(ubication)
+    let ubicationSearch =[];
+  try {
+    const allActiveWalkers = await User.findAll({
+      where: {
+        status: "active",
+      },
+    });
+    if (allActiveWalkers) {
+        const allUbication = await allActiveWalkers.map(w =>  w.ubication );
+        const allUbi= [...new Set(allUbication)]
+      //GET BY NAME
+      
+      if (ubication) {
+        
+          ubicationSearch = allUbi.filter(
+            (user) => user.toLowerCase().includes(ubication.toLowerCase())
+          );
+        }else{ubicationSearch = allUbi}
+
+          const ubicationOrdenAlfabetico = ubicationSearch.sort(function(a, b) {
+            if (a > b) {
+                return 1;
+            }
+            if (b > a) {
+                return -1;
+            }
+            return 0;
+        })
+
+        
+          res.status(200).send(ubicationSearch);
+         
+      
+    } else {
+      res.status(404).send("Not found");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -11,6 +11,7 @@ export const FILTER_SCHEDULE = "FILTER_SCHEDULE"
 export const FILTER_UBICATION = "FILTER_UBICATION"
 export const NEW_PASEADOR = 'NEW_PASEADOR'
 export const GET_BY_EMAIL = "GET_BY_EMAIL"
+export const UBICATION_MATCH= "UBICATION_MATCH"
 
 export function getByEmail(payload){
   return async function(dispatch){
@@ -155,3 +156,20 @@ export function FilterUbication( payload ) {
     payload
   }
 }
+
+export function ubicationMatch(ubication){
+  console.log(ubication)
+  return async function(dispatch){
+      let json;
+      
+      try{
+      
+          json = await axios.get(`http://localhost:3001/ubication?ubication=${ubication}`)
+      
+      console.log(json.data)
+      return dispatch({ type: UBICATION_MATCH, payload: json.data });
+      }catch(error){
+          return dispatch({ type: UBICATION_MATCH, payload: error });
+      }
+  }
+} 
