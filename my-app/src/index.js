25ff669fa20d7e0aedbf0d52fdf65473b0243f37
felipeballarino <felipeballarino97@gmail.com>
2021-10-05 13:47:00 -0300

    solucion errores

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,10 @@ import App from './App';
 import './index.css'
 import { Provider } from 'react-redux';
 import store from './store';
-import dotenv from 'dotenv'
-dotenv.config();
-
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+// import dotenv from 'dotenv'
+// import axios from 'axios'
+// dotenv.config();
+// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 
 ReactDOM.render(
