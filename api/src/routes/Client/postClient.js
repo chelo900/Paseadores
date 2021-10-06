c1dc5de5af5bddda4886f62840c6d3cbe9e9bc9b
cepentivolpi <ce.pentivolpi@gmail.com>
2021-10-06 11:47:13 -0300

    postClient

---
+++ api/src/routes/Client/postClient.js
@@ -0,0 +1,47 @@
+const { Router } = require("express");
+const router = Router();
+const { Client } = require("../../db");
+const bcryptjs = require("bcryptjs");
+
+router.post("/", async (req, res) => {
+  const {
+    ubication,
+    name,
+    surname,
+    dni,
+    image,
+    phone,
+    email,
+    password
+  } = req.body;
+
+  let passwordHash = await bcryptjs.hash(password, 8);
+
+  try {
+    const [client, created] = await Client.findOrCreate({
+      where: {
+        email: email,
+      },
+      defaults: {
+        ubication: ubication,
+        name: name,
+        dni: dni,
+        surname: surname,
+        image: image,
+        phone: phone,
+        password: passwordHash,
+      },
+    });
+    if (!created) {
+      return res
+        .status(200)
+        .json({ message: "Your email adress is already registered" });
+    }
+
+    res.status(201).json(client);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
