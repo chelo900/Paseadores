b8a7db29dbae12b0380996d0d38cd11d58cd1fa8
Martina-dP <martudelpino@gmail.com>
2021-10-07 00:09:35 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -12,13 +12,8 @@ const { conn } = require("./src/db.js");
 //asdasdas
 
 // Syncing all the models at once.
-<<<<<<< HEAD
 conn.sync({ force: false }).then(() => {
   server.listen({ port : process.env.PORT || 3001}, () => {
-=======
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
->>>>>>> 5478c30 (cambios)
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
