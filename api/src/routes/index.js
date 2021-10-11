f4b10203923b58ce43f1924de3703a4bba093553
Nicolas <76431460+Edenoor@users.noreply.github.com>
2021-10-11 15:32:28 -0300

    deploy (#4)
    * force false (#103)
    
    * Admin (#104)
    
    * Usuario Admin
    
    * Usuario Admin
    
    * mejorado los forms (#105)
    
    * please dejemos el force en false (#106)
    
    Co-authored-by: Claudio Pentivolpi <83839256+cepentivolpi@users.noreply.github.com>
    Co-authored-by: Gonzalo Cervan <76969287+Goncervan@users.noreply.github.com>

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+//const { Administrator} = require("../db");
 const get_details = require("./get_details");
 const postUser = require("./postUser");
 const putDetailsUser = require("./putDetailsUser");
@@ -9,16 +10,18 @@ const putHorarios = require("./putHorarios");
 const postImages = require("./postImages");
 const forgotPassword = require ("./forgotPassword")
 const newPassword = require ("./newPassword")
-const get_horarios = require ("../routes/FilterOrder/get_horarios")
 const get_ubication = require ("../routes/FilterOrder/get_ubication")
-const get_price = require ("../routes/FilterOrder/getPrice")
-const get_order = require ("../routes/FilterOrder/getOrder")
-const get_service = require ("../routes/FilterOrder/getService")
 const postClient = require ("./Client/postClient")
 const putClientePerfil = require ("../routes/Client/putClientePerfil")
 const putCliente = require ("../routes/Client/putClient")
 const detailsClient = require("../routes/Client/getDetailsCliente")
 const postPremium = require("./postPremium")
+const getWalkers = require("../routes/Administrator/getWalkers")
+const getClients = require("../routes/Administrator/getClients")
+const makeAdmin = require("../routes/Administrator/makeAdmin")
+const deleteUserAccount = require("../routes/Administrator/deleteUserAccount")
+const resetPassword = require("../routes/Administrator/resetPassword")
+const firstAdmin = require("../routes/Administrator/firstAdmin")
 
 const router = Router();
 
@@ -28,12 +31,8 @@ router.use("/createUser", postUser);
 router.use("/updateuser", putDetailsUser);
 router.use("/updateuserProfile", putDetailsProfile);
 router.use("/login", login);
-router.use("/walkers", get_horarios);
 router.use("/walkers", putHorarios);
 router.use("/ubication", get_ubication);
-router.use("/", get_price)
-router.use("/", get_order)
-router.use("/", get_service)
 router.use("/postimages", postImages);
 router.use("/forgotPassword",forgotPassword);
 router.use("/newPassword", newPassword);
@@ -42,5 +41,11 @@ router.use("/updateClientProfile", putClientePerfil);
 router.use("/updateCliente", putCliente);
 router.use("/Cliente", detailsClient);
 router.use("/updatePremium", postPremium);
+router.use("/getWalkers", getWalkers);
+router.use("/getClients", getClients);
+router.use("/makeAdmin", makeAdmin);
+router.use("/deleteUserAccount", deleteUserAccount);
+router.use("/resetPassword", resetPassword);
+router.use("/firstAdmin", firstAdmin);
 
 module.exports = router;
