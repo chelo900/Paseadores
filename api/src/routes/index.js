b8d588f305672b9b27dc4cc5f12b66a803a1f886
Nicolas <76431460+Edenoor@users.noreply.github.com>
2021-10-10 21:19:53 -0300

    force false (#103)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,11 +9,7 @@ const putHorarios = require("./putHorarios");
 const postImages = require("./postImages");
 const forgotPassword = require ("./forgotPassword")
 const newPassword = require ("./newPassword")
-const get_horarios = require ("../routes/FilterOrder/get_horarios")
 const get_ubication = require ("../routes/FilterOrder/get_ubication")
-const get_price = require ("../routes/FilterOrder/getPrice")
-const get_order = require ("../routes/FilterOrder/getOrder")
-const get_service = require ("../routes/FilterOrder/getService")
 const postClient = require ("./Client/postClient")
 const putClientePerfil = require ("../routes/Client/putClientePerfil")
 const putCliente = require ("../routes/Client/putClient")
@@ -28,12 +24,8 @@ router.use("/createUser", postUser);
 router.use("/updateuser", putDetailsUser);
 router.use("/updateuserProfile", putDetailsProfile);
 router.use("/login", login);
-router.use("/walkers", get_horarios);
 router.use("/walkers", putHorarios);
 router.use("/ubication", get_ubication);
-router.use("/", get_price)
-router.use("/", get_order)
-router.use("/", get_service)
 router.use("/postimages", postImages);
 router.use("/forgotPassword",forgotPassword);
 router.use("/newPassword", newPassword);
