5a94b693b896791c712c6858a68702b1cfdb04b8
Nicolas <76431460+Edenoor@users.noreply.github.com>
2021-10-15 14:44:09 -0300

    Google (#154)

--- my-app/src/Components/Login/Google_Auth.jsx
+++ my-app/src/Components/Login/Google_Auth.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { GoogleLogin } from "react-google-login";
 import axios from "axios";
-import Confetti from "react-confetti";
-import { ToastContainer, toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
 import "react-toastify/dist/ReactToastify.css";
+import Swal from "sweetalert2";
 
 const Log = () => {
   // States for the component:
+  const history = useHistory()
+  const dispatch = useDispatch()
+
   const [state, setState] = useState({
-    name: "",
+    validate: false,
+    id: "",
     email: "",
-    picture: "",
-    profile_loaded: false,
+    token: "",
     confetti: false,
+    profile_loaded: false
   });
 
   // On Failur of google login we get the reason for failur in an alert:
@@ -23,68 +28,134 @@ const Log = () => {
   // If successfull return of data from google we run this function:
   const googleResponse = async (response) => {
     // Check if a token was recieved and send it to our API:
-    console.log(response.tokenId)
-    // console.log(response.profileObj)
+    if (response.tokenId) {
 
-    const googleResponse = await axios.post("/google",{ token: response.tokenId })
-      console.log(googleResponse)
-    // if (response.tokenId) {
-    //   const googleResponse = await axios.post(
-    //     "/api/v1/user-auth",
-    //     { token: response.tokenId }
-    //   );
+    const body = await JSON.stringify({
+      "tokenId": response.tokenId
+    })
+
+
+    const googleResponse = await axios.post("/google", body, {
+      "headers": {
+      "content-type": "application/json",
+      },
+      })
+      // console.log("googleResponse", googleResponse)
+
     //   // Check if we have some result:
-    //   if (Object.keys(googleResponse.data.payload).length !== 0) {
+      // if (Object.keys(googleResponse.data.payload).length !== 0) {
     //     /*
     //       Get the following user details from our API and set them in the state:
     //       User Account Name
     //       User Email
     //       User Profile Picture for Google
     //     */
-    //     const { name, email, picture } = googleResponse.data.payload;
-    //     setState({
-    //       ...state,
-    //       name,
-    //       email,
-    //       picture,
-    //       profile_loaded: true,
-    //       confetti: true,
-    //     });
-    //     // Show a toast to the user letting them know that thelogin was successfull:
-    //     toast.success("You have logged into your google account!", {
-    //       position: "top-right",
-    //       autoClose: 5000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: false,
-    //       draggable: true,
-    //       progress: undefined,
-    //     });
-    //   }
-    // }
-  };
+        const { validate, id, email, token } = googleResponse.data
+
+       
+       await setState({
+          ...state,
+          validate: validate,
+          id: id,
+          email: email,
+          token: token,
+        });
+
+        
+        console.log(state)
+        
+        // const log = async () => {
+        //   const validation = await state.validate
+          
+        //        if (validation === true) {
+        //           await Swal.fire({
+        //                icon: 'success',
+        //                title: 'Welcome!',
+        //                showConfirmButton: false,
+        //                timer: 1500
+        //              })
+        //       history.push(`/walker/perfil/${state.id}`)
+        // //        localStorage.clear();
+        // //        localStorage.setItem("userValidate", state.validate);
+        // //        localStorage.setItem("userToken", state.token);
+        // //        localStorage.setItem("userId", state.id);
+        // //        localStorage.setItem("userWalker", state.walker);
+        // //        localStorage.setItem("userAdmin", state.admin);
+        // //        history.push(`/walker/perfil/${state.id}`);
+        // //        if (state.walker) {
+        // //        } else {
+        // //          history.push(`/cardsUsers`);
+        // //        }
+        //      } else if (state.validate === false) {
+        //       await Swal.fire({
+        //          icon: 'error',
+        //          title: 'Oops...',
+        //          text: 'Chequea tus crecenciales',
+        //          button:"Ok"
+        //        })
+        //        window.location.reload();
+        //      }
+        // //      // eslint-disable-next-line react-hooks/exhaustive-deps
+        //    }
+        //    log()
+
+       }
+      
+
+      }
+
+    
+
+    //   useEffect(async () => {
+   
+    //    if (state.validate === true) {
+    //     await Swal.fire({
+    //        icon: 'success',
+    //        title: 'Welcome!',
+    //        showConfirmButton: false,
+    //        timer: 1500
+    //      })
+    //      localStorage.clear();
+    //      localStorage.setItem("userValidate", state.validate);
+    //      localStorage.setItem("userToken", state.token);
+    //      localStorage.setItem("userId", state.id);
+    //      localStorage.setItem("userWalker", state.walker);
+    //      localStorage.setItem("userAdmin", state.admin);
+    //      history.push(`/walker/perfil/${state.id}`);
+    //      if (state.walker) {
+    //      } else {
+    //        history.push(`/cardsUsers`);
+    //      }
+    //    } else if (state.validate === false) {
+    //     await Swal.fire({
+    //        icon: 'error',
+    //        title: 'Oops...',
+    //        text: 'Chequea tus crecenciales',
+    //        button:"Ok"
+    //      })
+    //      window.location.reload();
+    //    }
+    //    // eslint-disable-next-line react-hooks/exhaustive-deps
+    //  }, []);
 
   // This will turn off the confetti raining down on the screen after 5 seconds of successfull login:
-  useEffect(() => {
-    setTimeout(() => {
-      setState({
-        ...state,
-        confetti: false,
-      });
-    }, 5000);
-  }, [state.profile_loaded]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setState({
+  //       ...state,
+  //       confetti: false,
+  //     });
+  //   }, 5000);
+  // }, [state.profile_loaded]);
+ 
+
 
 
 
   return (
+    ///////////////////////////////////////////
     <div>
-      {/* <script src="https://accounts.google.com/gsi/client" async defer></script>
-      
-      <div id="g_id_onload"
-         data-client_id="1003857961946-27cq7cl8j2867vtb0q9oikn26l2bnk65.apps.googleusercontent.com"
-         data-login_uri="https://your.domain/your_login_endpoint"
-         data-callback="handleCredentialResponse"
-         data-auto_prompt="false"> */}
+    
         <div>
           <GoogleLogin
             clientId="1003857961946-27cq7cl8j2867vtb0q9oikn26l2bnk65.apps.googleusercontent.com"
@@ -94,60 +165,11 @@ const Log = () => {
             cookiePolicy={'single_host_origin'}
           />
         </div>
-      {/* </div>
-      <div class="g_id_signin"
-         data-type="standard"
-         data-size="large"
-         data-theme="outline"
-         data-text="sign_in_with"
-         data-shape="rectangular"
-         data-logo_alignment="left">
-      </div> */}
     </div>
-      
+    ///////////////////////////////////////////
 
-  
-
-    // <div className="app">
-    //   {/* The Toast container */}
-    //   <ToastContainer
-    //     position="top-right"
-    //     autoClose={5000}
-    //     hideProgressBar={false}
-    //     newestOnTop={false}
-    //     closeOnClick
-    //     rtl={false}
-    //     pauseOnFocusLoss
-    //     draggable
-    //     pauseOnHover={false}
-    //   />
-    //   {/* Show login button when user not logged in */}
-    //   {!state.profile_loaded ? (
-    //     <div>
-    //       <GoogleLogin
-    //         clientId="CLIENT-IDXXXXXXXXXXXXXXXXXXXX"
-    //         buttonText="Login"
-    //         onSuccess={googleResponse}
-    //         onFailure={onFailure}
-    //       />
-    //     </div>
-    //   ) : (
-    //     // Show User details when logged in:
-    //     <div className="user-details">
-    //       {state.confetti ? (
-    //         // Confetti Component:
-    //         <Confetti width={window.innerWidth} height={window.innerHeight} />
-    //       ) : null}
-    //       <img
-    //         src={state.picture}
-    //         alt="profilePicture"
-    //         className="profile-picture"
-    //       />
-    //       <h3>{state.name}</h3>
-    //       <h3>{state.email}</h3>
-    //     </div>
-    //   )}
-    // </div>
+    
+    
   );
 };
 
